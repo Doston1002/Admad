32de62725d540a89014a3c6fa99e347f193bfbf3
Doston <polatovdoston1002@gmail.com>
2022-12-24 20:59:36 +0500

    header

--- Ahmad/js/main.js
+++ Ahmad/js/main.js
@@ -3,7 +3,7 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = ()=>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+    
 }
 let searchForm = document.querySelector('.search-form');
 
